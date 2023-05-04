d45b2476efba9a5e9db011baf42d7c2d8ff6d745
anosman <anosman6891@gmail.com>
2023-05-04 06:01:35 +0200

    no message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,64 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './component/footer/footer.component';
 import { HeaderComponent } from './component/header/header.component';
 import { HomeComponent } from './component/home/home.component';
+import { CloudTitleComponent } from './component/cloud-title/cloud-title.component';
+import { RecentTacosComponent } from './component/recents/recents.component';
+import { RouterModule, Routes } from '@angular/router';
+import { WrapsPipe } from './component/recents/WrapsPipe';
+import { NonWrapsPipe } from './component/recents/NonWrapsPipe';
+import { HttpClientModule } from '@angular/common/http';
+
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  /*{
+    path: 'login',
+    component: LoginComponent
+  },**/
+  {
+    path: 'recents',
+    component: RecentTacosComponent
+  },
+  /*{
+    path: 'specials',
+    component: SpecialsComponent
+  },
+  {
+    path: 'locations',
+    //component: LocationsComponent
+  },
+  {
+    path: 'design',
+    //component: DesignComponent
+  },
+  {
+    path: 'cart',
+    //component: CartComponent
+  },*/
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    CloudTitleComponent,
+    RecentTacosComponent,
+    NonWrapsPipe,
+    WrapsPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/component/cloud-title/cloud-title.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cloud-title',
+  templateUrl: './cloud-title.component.html',
+  styleUrls: ['./cloud-title.component.css']
+})
+export class CloudTitleComponent {
+
+}
